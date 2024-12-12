90fb7bf15bd55c68af59db71e6fdbacc1bcaa51b
Juliet Adjeiwaa <julietadjeiwaa@Jacobs-MacBook-Pro.local>
2024-12-12 18:45:27 +0000

    second commit

---
+++ T02 - Getting Started With Your Bootcamp/hello_world.py
@@ -0,0 +1,19 @@
+# Here we request the user's name
+
+name = input("please type your name here: ")
+
+# Here we print "hello" and the user's name
+
+print("hello "+name)
+
+# Here we request the user's age
+
+age = input("please type your age here: ")
+
+# Here we print "you are" and the user's age
+
+print("you are "+age)
+
+# Here we print "hello world"
+
+print("Hello World!")
