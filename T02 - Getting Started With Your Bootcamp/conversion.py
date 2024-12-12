960c170508049008f4b08a56b80f232115dc748b
Juliet Adjeiwaa <julietadjeiwaa@Jacobs-MacBook-Pro.local>
2024-12-12 18:51:58 +0000

    third commit

---
+++ T02 - Getting Started With Your Bootcamp/conversion.py
@@ -0,0 +1,19 @@
+num1 = 99.23 # this is a "float"
+num2 = 23 # this is an "integer"
+num3 = 150 # this is an "integer"
+string1 = "100"
+
+# Convert num1 (float) to an integer
+num1_converted = int(num1)
+#Convert num2 (integer) to a float
+num2_converted = float(num2)
+#Convert num3 (integer) to a string
+num3_converted = str(num3)
+#Convert string1 (string) to an integer
+string1_converted = int(string1)
+
+# Printing the variables
+print(num1_converted)
+print(num2_converted)
+print(num3_converted)
+print(string1_converted)
